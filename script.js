453a2f60c0180eab237bf550bee2c66ce94fd3de
Karlita2227 <karlaoglivie@gmail.com>
2025-03-25 19:52:58 -0700

    Update script.js

--- script.js
+++ script.js
@@ -8,28 +8,17 @@ let operations = {
 };
 
 function calculate() {
-	// stores the first number entered's value in our JavaScript
-	// while converting it to a decimal using parseFloat()
 	let num1 = parseFloat(document.getElementById("num1").value);
-	// stores the second number entered's value in our JavaScript
-	// while converting it to a decimal using parseFloat()
 	let num2 = parseFloat(document.getElementById("num2").value);
-	// stores the mathematical operator we've selected from the dropdown menu
-	let operator = document.getElementById("operations").value;
 
 
 	let result;
-
 	
-    if (isNaN(num1) || isNaN(num2)) {
+        if (isNaN(num1) || isNaN(num2)) {
 	
-        result = "Enter a valid number";
+            result = "Enter a valid number";
     } else {
-
         result = operations[operator](num1, num2);
-    }
-
-
-	// we then change that within our HTML using innerText
-	document.getElementById("result").innerText = "Result: " + result;
-}
+	}
+let operator = document.getElementById("operations").value;
+		
