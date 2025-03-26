897b305d85baf3f9cdfbf22686927388a7a62f90
Karlita2227 <karlaoglivie@gmail.com>
2025-03-25 19:39:58 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+let operations = {
+    "+": (a, b) => a + b,
+    "-": (a, b) => a - b,
+    "
+};
 
+function calculate() {
+	// stores the first number entered's value in our JavaScript
+	// while converting it to a decimal using parseFloat()
+	let num1 = parseFloat(document.getElementById("num1").value);
+	// stores the second number entered's value in our JavaScript
+	// while converting it to a decimal using parseFloat()
+	let num2 = parseFloat(document.getElementById("num2").value);
+	// stores the mathematical operator we've selected from the dropdown menu
+	let operator = document.getElementById("operator").value;
+
+	// variable that will represent the result after it's been calculated
+	let result;
+
+	// checks if either num1 or num2 is anything other than a numerical value
+    if (isNaN(num1) || isNaN(num2)) {
+	// if either ARE anything other than acceptable numerical values
+	// result will be the following string
+        result = "Enter a valid number";
+    } else {
+	// otherwise, result assign itself to be whatever the two nums evaluate to with the selected operator
+        result = operations[operator](num1, num2);
+    }
+
+
+	// we then change that within our HTML using innerText
+	document.getElementById("result").innerText = "Result: " + result;
+}
