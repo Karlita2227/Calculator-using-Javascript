0d51ad95be19d8f158dc6b38e2712505757c90b5
Karlita2227 <karlaoglivie@gmail.com>
2025-03-25 19:56:51 -0700

    Update script.js

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ let operations = {
 function calculate() {
 	let num1 = parseFloat(document.getElementById("num1").value);
 	let num2 = parseFloat(document.getElementById("num2").value);
-
-
+        let operator = document.getElementById("operator").value;
+	
 	let result;
 	
         if (isNaN(num1) || isNaN(num2)) {
@@ -20,5 +20,7 @@ function calculate() {
     } else {
         result = operations[operator](num1, num2);
 	}
-let operator = document.getElementById("operations").value;
+	
+document.getElementById("result").innerText = "Result: " + result;
+}
 		
